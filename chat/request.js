7733263839099309b54cb41b679eafc36cb2285a
Tuan PHAM <phamtuantkt@gmail.com>
2017-10-24 04:22:20 +0700

    sdads

--- chat/request.js
+++ chat/request.js
@@ -42,57 +42,58 @@ function send(mytext) {
         headers: {"Authorization": "Bearer 1491fc0d408c4bfbb6d4e53773a14940"},
         data: JSON.stringify({ query: text, sessionId: "1491fc0d408" }),
         success: function(data) {
-            setResponse(JSON.stringify(data));
+//            setResponse(JSON.stringify(data));
+            speech = $.parseJSON(JSON.stringify(data));
+            var replies = speech.result.fulfillment.messages;
+            nb_reply = replies.length
+            for (i = 0; i < nb_reply; i++) {
+
+                var html = replies[i]["speech"];
+                var div = document.createElement("div");
+                div.innerHTML = html;
+
+                $.ajax({
+                    type: "POST",
+                    url: "http://api.openfpt.vn/text2speech/v4",
+                    contentType: "application/json; charset=utf-8",
+                    dataType: "json",
+                    headers: {
+                            "api_key": "b7891e7b46764220b73911ed479f3c7f",
+                            "speed": 2,
+                            "voice": "male",
+                            "prosody": 1,
+                            },
+                    data: div.innerText,
+                    success: function(data) {
+                        //setSound(JSON.stringify(data));
+                        speech = $.parseJSON(JSON.stringify(data));
+                        var url = speech.async;
+                        alert(url)
+                        var audio = new Audio(url);
+                        audio.play();
+                        },
+                });
+
+                var doc = document.getElementById('bot').innerHTML;
+                document.getElementById('bot').innerHTML = doc + "<div class='calloutbig' id='callout'><img src='chatbot.png' width='50px' height='50px' class='circular--square' style='float: left;' /><div class='calloutright'>" + replies[i]["speech"] + "</div><div class='message-from message-from-bot'>"+ new Date().toLocaleTimeString() +"</div></div>";
+                var doc = document.getElementById('bot').lastElementChild.innerHTML;
+                var objDiv = document.getElementById("bot");
+                objDiv.scrollTop = objDiv.scrollHeight;
+                }
         },
     });
 
     $('#input').val("");
 }
 
-function setResponse(val) {
-    speech = $.parseJSON(val);
-    var replies = speech.result.fulfillment.messages;
-    nb_reply = replies.length
-    for (i = 0; i < nb_reply; i++) {
+//function setResponse(val) {
+//
+//}
 
-        var html = replies[i]["speech"];
-        var div = document.createElement("div");
-        div.innerHTML = html;
-
-        $.ajax({
-            type: "POST",
-            url: "http://api.openfpt.vn/text2speech/v4",
-            contentType: "application/json; charset=utf-8",
-            dataType: "json",
-            headers: {
-                    "api_key": "b7891e7b46764220b73911ed479f3c7f",
-                    "speed": 2,
-                    "voice": "male",
-                    "prosody": 1,
-                    },
-            data: div.innerText,
-            success: function(data) {
-//                setSound(JSON.stringify(data));
-                speech = $.parseJSON(JSON.stringify(data));
-                var url = speech.async;
-                alert(url)
-                var audio = new Audio(url);
-                audio.play();
-                },
-        });
-
-        var doc = document.getElementById('bot').innerHTML;
-        document.getElementById('bot').innerHTML = doc + "<div class='calloutbig' id='callout'><img src='chatbot.png' width='50px' height='50px' class='circular--square' style='float: left;' /><div class='calloutright'>" + replies[i]["speech"] + "</div><div class='message-from message-from-bot'>"+ new Date().toLocaleTimeString() +"</div></div>";
-        var doc = document.getElementById('bot').lastElementChild.innerHTML;
-        var objDiv = document.getElementById("bot");
-        objDiv.scrollTop = objDiv.scrollHeight;
-        }
-}
-
-function setSound(val){
-    speech = $.parseJSON(val);
-    var url = speech.async;
-    alert(url)
-    var audio = new Audio(url);
-    audio.play();
-}
+//function setSound(val){
+//    speech = $.parseJSON(val);
+//    var url = speech.async;
+//    alert(url)
+//    var audio = new Audio(url);
+//    audio.play();
+//}
